3e664ee8ddd4d0801ffe4ffc5d72cf94342e2bc8
Jamie Pond <jamiepond259@gmail.com>
2023-12-01 23:06:28 +0000

    fuck yeah

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
-# install boring things we probably need
-sudo apt update && sudo apt install -y build-essential
 
+# install boring things we probably need
+sudo apt update && sudo apt install -y build-essential nodejs npm unzip zip
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# go
+sudo snap install go --classic
+
+
+# setup git
+git config --global user.email "jamiepond259@gmail.com"
+git config --global user.name "Jamie Pond"
+
+# install docker
+# 1. add gpg key
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# 2. do the install
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+rm -rf ~/jamie-config
+git clone https://github.com/jamierpond/.config ~/jamie-config
+cp -r ~/jamie-config/* ~/.config
+
+# install nvims
+sudo snap install nvim --classic
+
+# todo just make this using a token
 # install gh cli
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
@@ -14,39 +48,3 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 gh auth login
 gh auth setup-git
 
-git config --global user.email "jamiepond259@gmail.com"
-git config --global user.name "Jamie Pond"
-
-# install docker
-# # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-# install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-echo "You now need to authorize the github cli with your account"
-
-git clone https://github.com/jamierpond/.config ~/jamie-config
-cp -r ~/jamie-config/* ~/.config
-
-# install typscript and node
-curl -fsSL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-sudo apt-get install -y nodejs
-npm install -g typescript
-
-sudo snap install go --classic
-
-wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-tar xzf nvim-linux64.tar.gz
-sudo mv nvim-linux64/bin/nvim /usr/bin/nvim
