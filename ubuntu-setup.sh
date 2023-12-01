d45d209b97fdf53015ac5a09210a745bcf3eee63
Jamie Pond <jamiepond259@gmail.com>
2023-12-01 23:13:07 +0000

    fzf

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install boring things we probably need
-sudo apt update && sudo apt install -y build-essential nodejs npm unzip zip
+sudo apt update && sudo apt install -y build-essential nodejs npm unzip zip fzf
 
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
