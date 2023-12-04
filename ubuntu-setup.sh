7388b32167bd7bb770f301cd5ce4da7a9ba5bcdc
Jamie Pond <jamiepond259@gmail.com>
2023-12-04 21:02:17 +0000

    yup

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,13 +1,51 @@
 #!/bin/bash
 set -e
 
+# *** You need libgpg-error to build this program.
+# **  This library is for example available at
+# ***   https://gnupg.org/ftp/gcrypt/gpgrt
+# *** (at least version 1.46 is required.)
+# ***
+# configure:
+# ***
+# *** You need libgcrypt to build this program.
+# **  This library is for example available at
+# ***   https://gnupg.org/ftp/gcrypt/libgcrypt/
+# *** (at least version 1.9.1 (API 1) is required.)
+# ***
+# configure:
+# ***
+# *** You need libassuan to build this program.
+# *** This library is for example available at
+# ***   https://gnupg.org/ftp/gcrypt/libassuan/
+# *** (at least version 2.5.0 (API 2) is required).
+# ***
+# configure:
+# ***
+# *** You need libksba to build this program.
+# *** This library is for example available at
+# ***   https://gnupg.org/ftp/gcrypt/libksba/
+# *** (at least version 1.6.3 using API 1 is required).
+# ***
+# configure:
+# ***
+# *** It is now required to build with support for the
+# *** New Portable Threads Library (nPth). Please install this
+# *** library first.  The library is for example available at
+# ***   https://gnupg.org/ftp/gcrypt/npth/
+
 # install boring things we probably need
 sudo apt update && sudo apt install -y build-essential nodejs npm unzip zip \
-  fzf tmux pkg-config protobuf-compiler cmake
+  fzf tmux pkg-config protobuf-compiler cmake linux-libc-dev clang git-secret \
+  libksba-dev libassuan-dev libgpg-error-dev libgcrypt-dev libnpth-dev
 sudo apt upgrade
 
 # tmux alias, so we can use our config
 echo "alias tmux='tmux -f ~/.config/tmux/tmux.conf'" >> ~/.bashrc
+
+# install docker
+# 1. add gpg key
+sudo snap install docker
 
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -18,20 +56,6 @@ sudo snap install go --classic
 # setup git
 git config --global user.email "jamiepond259@gmail.com"
 git config --global user.name "Jamie Pond"
-
-# install docker
-# 1. add gpg key
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-# 2. do the install
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
 
 rm -rf ~/jamie-config
 git clone https://github.com/jamierpond/.config ~/jamie-config
