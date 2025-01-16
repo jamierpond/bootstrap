c1de5457795bf09a4fedc8228f627c0751c88846
Jamie Pond <jamiepond259@gmail.com>
2025-01-16 06:31:08 +0000

    also install ranger

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -4,7 +4,7 @@ set -e
 # install boring things we probably need
 sudo apt update && sudo apt install -y build-essential nodejs npm unzip zip \
   fzf ripgrep snapd ffmpeg sox libsox-dev pkg-config protobuf-compiler cmake linux-libc-dev clang git-secret \
-  libomp-dev
+  libomp-dev ranger
 sudo apt upgrade
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
